7630e9fd64eff00f44179afb665e6615c00e0265
rbt15 <camontogot@gmail.com>
2021-02-18 15:09:01 +0300

    login method and jwt

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,7 @@
 function errorHandler(err, req, res, next) {
   switch (true) {
-    case typeof err === "string":
-      const is404 = err.toLowerCase().endsWith("not found");
-      const statusCode = is404 ? 404 : 400;
-      return res.status(statusCode).json({ message: err });
+    case err.statusCod !== "":
+      return res.status(err.statusCode).json(err);
     case err.name === "ValidationError":
       return res.status(400).json({ message: err.message });
     case err.name === "MongoError":
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
 
 router.get("/", (req, res) => {
   res.send("Auth");
@@ -9,7 +11,25 @@ router.post("/register", async (req, res, next) => {
   try {
     const user = new User(req.body);
     const result = await user.save();
-    res.json(result);
+    const token = jwt.sign({ user: user._id }, "SECRET", {
+      expiresIn: "365 days",
+    });
+    res.json({ user: result._id, token: token });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) throw createError.NotFound("User not found");
+    const isMatch = await user.isValidPassword(req.body.password);
+    if (!isMatch) throw createError.Unauthorized("Username/password not valid");
+    const token = jwt.sign({ user: user._id }, "SECRET", {
+      expiresIn: "365 days",
+    });
+    res.send({ token: token });
   } catch (error) {
     next(error);
   }
