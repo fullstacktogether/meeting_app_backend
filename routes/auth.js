3597e955d7fde26425b0dbaf6b906a06ce7da25d
BerkkanB <butun18@itu.edu.tr>
2021-02-19 14:46:51 +0300

    /me Patch, username update added

--- routes/auth.js
+++ routes/auth.js
@@ -39,7 +39,11 @@ router.get("/me",authMiddleware,(req,res,next)=>{
     res.json(req.user)
 });
 
-
+router.patch("/me",authMiddleware,async(req,res)=>{
+  const userId=req.user._id
+  const updatedUser = await User.findByIdAndUpdate(userId,{username:req.body.username},{new:true})
+  res.json(updatedUser)
+})
 
 
 
